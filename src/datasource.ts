f80378ac5200003709ca4c458e347a563273f6a5
Dario Segger <dario.segger@trivago.com>
2018-05-17 15:18:04 +0200

    add datasource test

--- src/datasource.ts
+++ src/datasource.ts
@@ -2,15 +2,44 @@
 
 import _ from 'lodash';
 
+export class GenericDatasource {
+
+
+
+}
+
 export default class ChangeMyNameDatasource {
-  id: number;
   name: string;
+  url: string;
+  authToken: string;
 
   /** @ngInject */
   constructor(instanceSettings, private backendSrv, private templateSrv, private $q) {
     this.name = instanceSettings.name;
-    this.id = instanceSettings.id;
+    this.url = 'https://www.googleapis.com/bigquery/v2/projects/chrome-ux-report/datasets/';
+    this.authToken = instanceSettings.authToken;
+
   }
+
+  doRequest(options) {
+    options.url = this.url;
+    options.headers = {
+      Authorization: this.authToken,
+    };
+    return this.backendSrv.datasourceRequest(options);
+  }
+
+  testDatasource() {
+    return this.doRequest({
+      url: this.url + '/',
+      method: 'GET',
+    }).then(response => {
+      if (response.status === 200) {
+        return { status: "success", message: "Data source is working", title: "Success" };
+      }
+    });
+  }
+
 
   query(options) {
     throw new Error("Query Support not implemented yet.");
@@ -25,6 +54,13 @@ export default class ChangeMyNameDatasource {
   }
 
   testDatasource() {
+    if (this.basicAuth || this.withCredentials) {
+      options.withCredentials = true;
+    }
+    if (this.basicAuth) {
+      options.headers = {
+        Authorization: this.basicAuth,
+};
     return this.$q.when({
       status: 'error',
       message: 'Data Source is just a template and has not been implemented yet.',
