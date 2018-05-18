9b77712157eb75ecd05fa6571a5eb3e82d1929fd
Dario Segger <dario.segger@trivago.com>
2018-05-18 11:07:22 +0200

    build

--- dist/query_ctrl.js
+++ dist/query_ctrl.js
@@ -25,16 +25,17 @@ System.register(['lodash', 'app/plugins/sdk'], function(exports_1) {
                     this.target.format = this.target.format || 'time_series';
                     this.target.alias = '';
                     this.formats = [{ text: 'Time series', value: 'time_series' }, { text: 'Table', value: 'table' }];
+                    /* Not used atm
                     if (!this.target.rawSql) {
-                        // special handling when in table panel
-                        if (this.panelCtrl.panel.type === 'table') {
-                            this.target.format = 'table';
-                            this.target.rawSql = 'SELECT 1';
-                        }
-                        else {
-                            this.target.rawSql = defaultQuery;
-                        }
+                      // special handling when in table panel
+                      if (this.panelCtrl.panel.type === 'table') {
+                        this.target.format = 'table';
+                        this.target.rawSql = 'SELECT 1';
+                      } else {
+                        this.target.rawSql = defaultQuery;
+                      }
                     }
+                    */
                     this.panelCtrl.events.on('data-received', this.onDataReceived.bind(this), $scope);
                     this.panelCtrl.events.on('data-error', this.onDataError.bind(this), $scope);
                 }
