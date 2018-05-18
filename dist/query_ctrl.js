7958cb39d564192a8ac9cfd537658906edb5d7aa
Dario Segger <dario.segger@trivago.com>
2018-05-18 07:57:11 +0200

    test query editor panel

--- dist/query_ctrl.js
+++ dist/query_ctrl.js
@@ -1,12 +1,12 @@
 ///<reference path="../node_modules/grafana-sdk-mocks/app/headers/common.d.ts" />
-System.register(['lodash', 'app/plugins/sdk', './css/query_editor.css!'], function(exports_1) {
+System.register(['lodash', 'app/plugins/sdk'], function(exports_1) {
     var __extends = (this && this.__extends) || function (d, b) {
         for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
         function __() { this.constructor = d; }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
     var lodash_1, sdk_1;
-    var BigQueryQueryCtrl;
+    var defaultQuery, BigQueryQueryCtrl;
     return {
         setters:[
             function (lodash_1_1) {
@@ -14,25 +14,46 @@ System.register(['lodash', 'app/plugins/sdk', './css/query_editor.css!'], functi
             },
             function (sdk_1_1) {
                 sdk_1 = sdk_1_1;
-            },
-            function (_1) {}],
+            }],
         execute: function() {
+            defaultQuery = "SELECT\n  UNIX_TIMESTAMP(<time_column>) as time_sec,\n  <value column> as value,\n  <series name column> as metric\nFROM <table name>\nWHERE $__timeFilter(time_column)\nORDER BY <time_column> ASC\n";
             BigQueryQueryCtrl = (function (_super) {
                 __extends(BigQueryQueryCtrl, _super);
                 /** @ngInject **/
-                function BigQueryQueryCtrl($scope, $injector, templateSrv) {
+                function BigQueryQueryCtrl($scope, $injector) {
                     _super.call(this, $scope, $injector);
-                    this.templateSrv = templateSrv;
-                    this.defaults = {};
-                    lodash_1.default.defaultsDeep(this.target, this.defaults);
-                    this.target.target = this.target.target || 'select metric';
-                    this.target.type = this.target.type || 'timeserie';
+                    this.target.format = this.target.format || 'time_series';
+                    this.target.alias = '';
+                    this.formats = [{ text: 'Time series', value: 'time_series' }, { text: 'Table', value: 'table' }];
+                    if (!this.target.rawSql) {
+                        // special handling when in table panel
+                        if (this.panelCtrl.panel.type === 'table') {
+                            this.target.format = 'table';
+                            this.target.rawSql = 'SELECT 1';
+                        }
+                        else {
+                            this.target.rawSql = defaultQuery;
+                        }
+                    }
+                    this.panelCtrl.events.on('data-received', this.onDataReceived.bind(this), $scope);
+                    this.panelCtrl.events.on('data-error', this.onDataError.bind(this), $scope);
                 }
-                BigQueryQueryCtrl.prototype.getOptions = function (query) {
-                    return this.datasource.metricFindQuery(query || '');
+                BigQueryQueryCtrl.prototype.onDataReceived = function (dataList) {
+                    this.lastQueryMeta = null;
+                    this.lastQueryError = null;
+                    var anySeriesFromQuery = lodash_1.default.find(dataList, { refId: this.target.refId });
+                    if (anySeriesFromQuery) {
+                        this.lastQueryMeta = anySeriesFromQuery.meta;
+                    }
                 };
-                BigQueryQueryCtrl.prototype.onChangeInternal = function () {
-                    this.panelCtrl.refresh(); // Asks the panel to refresh data.
+                BigQueryQueryCtrl.prototype.onDataError = function (err) {
+                    if (err.data && err.data.results) {
+                        var queryRes = err.data.results[this.target.refId];
+                        if (queryRes) {
+                            this.lastQueryMeta = queryRes.meta;
+                            this.lastQueryError = queryRes.error;
+                        }
+                    }
                 };
                 BigQueryQueryCtrl.templateUrl = 'partials/query.editor.html';
                 return BigQueryQueryCtrl;
--- dist/query_ctrl.ts
+++ dist/query_ctrl.ts
@@ -2,29 +2,78 @@
 
 import _ from 'lodash';
 import {QueryCtrl} from 'app/plugins/sdk';
-import './css/query_editor.css!';
+//import './css/query_editor.css!';
+
+export interface BigQuery {
+  refId: string;
+  format: string;
+  alias: string;
+  rawSql: string;
+}
+
+export interface QueryMeta {
+  sql: string;
+}
+
+const defaultQuery = `SELECT
+  UNIX_TIMESTAMP(<time_column>) as time_sec,
+  <value column> as value,
+  <series name column> as metric
+FROM <table name>
+WHERE $__timeFilter(time_column)
+ORDER BY <time_column> ASC
+`;
 
 export class BigQueryQueryCtrl extends QueryCtrl {
   static templateUrl = 'partials/query.editor.html';
 
-  defaults = {
-  };
+  showLastQuerySQL: boolean;
+  formats: any[];
+  target: BigQuery;
+  lastQueryMeta: QueryMeta;
+  lastQueryError: string;
+  showHelp: boolean;
 
   /** @ngInject **/
-  constructor($scope, $injector, private templateSrv) {
+
+  constructor($scope, $injector) {
     super($scope, $injector);
 
-    _.defaultsDeep(this.target, this.defaults);
+    this.target.format = this.target.format || 'time_series';
+    this.target.alias = '';
+    this.formats = [{ text: 'Time series', value: 'time_series' }, { text: 'Table', value: 'table' }];
 
-    this.target.target = this.target.target || 'select metric';
-    this.target.type = this.target.type || 'timeserie';
+    if (!this.target.rawSql) {
+      // special handling when in table panel
+      if (this.panelCtrl.panel.type === 'table') {
+        this.target.format = 'table';
+        this.target.rawSql = 'SELECT 1';
+      } else {
+        this.target.rawSql = defaultQuery;
+      }
+    }
+
+    this.panelCtrl.events.on('data-received', this.onDataReceived.bind(this), $scope);
+    this.panelCtrl.events.on('data-error', this.onDataError.bind(this), $scope);
   }
 
-  getOptions(query) {
-    return this.datasource.metricFindQuery(query || '');
+  onDataReceived(dataList) {
+    this.lastQueryMeta = null;
+    this.lastQueryError = null;
+
+    let anySeriesFromQuery = _.find(dataList, { refId: this.target.refId });
+    if (anySeriesFromQuery) {
+      this.lastQueryMeta = anySeriesFromQuery.meta;
+    }
   }
 
-  onChangeInternal() {
-    this.panelCtrl.refresh(); // Asks the panel to refresh data.
+  onDataError(err) {
+    if (err.data && err.data.results) {
+      let queryRes = err.data.results[this.target.refId];
+      if (queryRes) {
+        this.lastQueryMeta = queryRes.meta;
+        this.lastQueryError = queryRes.error;
+      }
+    }
   }
 }
